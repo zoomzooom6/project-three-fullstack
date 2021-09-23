3ed818677fb839fe9e93bc37dc77982def76bd6f
zoomzoom6 <tonyhuang107@live.ca>
2021-09-23 17:34:20 -0400

    add production parameters for server.js file

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}`);
